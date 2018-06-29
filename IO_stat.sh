42ff8c430d26546e02cdd04acb6939bd3ac19bf9
Ankit Sharma <TheAnkitSharma13@gmail.com>
2018-06-29 15:45:02 +0530

    Update IO_stat.sh

--- IO_stat.sh
+++ IO_stat.sh
@@ -4,4 +4,5 @@ wrqm=`iostat -hx | grep sd | awk '{sum+=$3}END{print sum;}'`
 rsec=`iostat -hx | grep sd | awk '{sum+=$6}END{print sum;}'`
 wsec=`iostat -hx | grep sd | awk '{sum+=$7}END{print sum;}'`
 svctm=`iostat -hx | grep sd | awk '{sum+=$13}END{print sum;}'`
-echo -e "{ \"Total rrpm\":\"$rrpm\",\"Total wrqm\":\"$wrqm\",\"Total rsec\":\"$rsec\",\"Total wsec\":\"$wsec\",\"Total svctm\":\"$svctm\" }"
+echo "rrpm,wrqm,rsec,wsec,svctm"
+echo -e "$rrpm,$wrqm,$rsec,$wsec,$svctm"
